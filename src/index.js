bbad757ab548e7e481b6d350a29769f57c91eaee
Limin Chen <clmkathy@gmail.com>
2024-03-08 00:09:58 +1000

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import App from './App-Kathy'
 
-import App from "./App-Kathy";
-
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
